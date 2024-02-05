743c9b52972d0f1ec1a02d2784c4809568f2c4df
Tapio Anttila <tapio-a@hotmail.com>
2024-02-05 13:13:45 +0200

    Add source files

---
+++ Assets/Scripts/Connection.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Connection : MonoBehaviour
+{
+    public string connectionName;
+    public string description;
+    public Location location;
+    public bool connectionEnabled;
+}
